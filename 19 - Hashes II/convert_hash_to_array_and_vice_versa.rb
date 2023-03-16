512257c73e7af636a26f24768ca7805bc0b0ec59
JRSkates <skatesproduction@gmail.com>
2023-03-16 12:57:19 +0000

    Updated Hash Lesson

--- 19 - Hashes II/convert_hash_to_array_and_vice_versa.rb
+++ 19 - Hashes II/convert_hash_to_array_and_vice_versa.rb
@@ -4,11 +4,11 @@ spice_girls = {scary: "Melanie Brown",
                ginger: "Geri Halliwell",
                posh: "Victoria Beckham"}
 
-#p spice_girls.to_a.flatten
+p spice_girls.to_a.flatten
 
 power_rangers = [
   [:red, "Jason"], [:black, "Zack"],
   [:blue, "Billy"], [:yellow, "Trini"],
   [:pink, "Kimberly"]]
 
-p power_rangers.to_h.class
+p power_rangers.to_h
